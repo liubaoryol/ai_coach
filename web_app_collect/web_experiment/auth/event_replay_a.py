416c65870ed144e2d2d5d5cf28a047cb36104f86
Sangwon Seo <prizeworthy3@gmail.com>
2022-08-10 03:35:04 -0500

    update web_app_collect according to visualization logic change

--- web_app_collect/web_experiment/auth/event_replay_a.py
+++ web_app_collect/web_experiment/auth/event_replay_a.py
@@ -1,35 +1,48 @@
 from web_experiment import socketio
-from ai_coach_domain.box_push.maps import EXP1_MAP
 from flask import (request, session)
 import web_experiment.experiment1.events_impl as event_impl
 from web_experiment.auth.util import update_canvas
 
-GRID_X = EXP1_MAP["x_grid"]
-GRID_Y = EXP1_MAP["y_grid"]
 TOGETHER_NAMESPACE = '/together'
+TASK_TYPE = event_impl.TASK_A
+
 
 # TOGETHER_NAMESPACE
 @socketio.on('connect', namespace=TOGETHER_NAMESPACE)
 def initial_canvas():
-    event_impl.initial_canvas(GRID_X, GRID_Y)
-    update_canvas(request.sid, TOGETHER_NAMESPACE, True)
+  event_impl.initial_canvas(session['session_name'], TASK_TYPE)
+  update_canvas(request.sid,
+                TOGETHER_NAMESPACE,
+                True,
+                is_movers_domain=TASK_TYPE)
+
 
 @socketio.on('next', namespace=TOGETHER_NAMESPACE)
 def next_index():
-    if session['index'] < (session['max_index'] - 1):
-        session['index'] += 1
-        update_canvas(request.sid, TOGETHER_NAMESPACE, True)
+  if session['index'] < (session['max_index'] - 1):
+    session['index'] += 1
+    update_canvas(request.sid,
+                  TOGETHER_NAMESPACE,
+                  True,
+                  is_movers_domain=TASK_TYPE)
+
 
 @socketio.on('prev', namespace=TOGETHER_NAMESPACE)
 def prev_index():
-    if session['index'] > 0:
-        session['index'] -= 1
-        update_canvas(request.sid, TOGETHER_NAMESPACE, True)
+  if session['index'] > 0:
+    session['index'] -= 1
+    update_canvas(request.sid,
+                  TOGETHER_NAMESPACE,
+                  True,
+                  is_movers_domain=TASK_TYPE)
+
 
 @socketio.on('index', namespace=TOGETHER_NAMESPACE)
-def next_index(msg):
-    idx = int(msg['index'])
-    if (idx <= (session['max_index'] - 1) and idx >= 0):
-        session['index'] = idx
-        update_canvas(request.sid, TOGETHER_NAMESPACE, True)
-    
+def goto_index(msg):
+  idx = int(msg['index'])
+  if (idx <= (session['max_index'] - 1) and idx >= 0):
+    session['index'] = idx
+    update_canvas(request.sid,
+                  TOGETHER_NAMESPACE,
+                  True,
+                  is_movers_domain=TASK_TYPE)
