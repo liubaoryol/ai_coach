00057c9b9da3eac333f03c381eea52e1f4484a4d
bh39 <bh39@rice.edu>
2022-09-29 11:12:46 -0500

    thought bubble

--- web_app_v2/config.py
+++ web_app_v2/config.py
@@ -7,6 +7,6 @@ DATABASE = "./data/tw2020_db/tw2020.sqlite"
 SURVEY_PATH = "./data/tw2020_survey"
 TRAJECTORY_PATH = "./data/tw2020_trajectory"
 LATENT_PATH = "./data/tw2020_latent"
-EXP_TYPE = "data_collection"  # data_collection | intervention
+EXP_TYPE = "intervention"  # data_collection | intervention
 
 DEBUG = False
