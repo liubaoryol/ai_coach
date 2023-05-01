b7ccb5f6b4cfd98a7107e2e64f902f7d27b44581
Sangwon Seo <prizeworthy3@gmail.com>
2023-04-30 19:11:32 -0500

    add test script

--- test_algs/run_exps2.sh
+++ test_algs/run_exps2.sh
@@ -11,5 +11,5 @@
 python3 test_algs/run_algs.py --alg_name gail --env_type mujoco \
         --env_name Hopper-v2 --use_option True --use_c_in_discriminator True \
         --n_demo 1000 --device "cuda:0" --use_state_filter False --tag option-gail-1k \
-        --data_path "experts/Hopper-v2_25.pkl" --max_explore_step 1e6
+        --data_path "experts/Hopper-v2_25.pkl" --max_explore_step 3e6
 
