e5fb0d5d23abde5223de2a587352218f7b8ba9fa
Sangwon Seo <prizeworthy3@gmail.com>
2023-04-30 23:18:30 -0500

    change run_exp

--- test_algs/run_exps.sh
+++ test_algs/run_exps.sh
@@ -64,5 +64,5 @@ python3 test_algs/run_algs.py --alg_name miql_v2 --env_type mujoco \
 python3 test_algs/run_algs.py --alg_name miql_v2 --env_type mujoco \
         --env_name Hopper-v2 --n_demo 1000 --device "cuda:0" \
         --bounded_actor True --tag miqlv2-1k-a3c1 --use_prev_action False \
-        --use_state_filter False --data_path "experts/Hopper-v2_25.pkl" \ 
+        --use_state_filter False --data_path "experts/Hopper-v2_25.pkl" \
         --num_actor_update 3 --num_critic_update 1
