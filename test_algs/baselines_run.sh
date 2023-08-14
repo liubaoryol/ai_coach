631854788b992e024f5f7cb3501216d222ab9df3
Sangwon Seo <prizeworthy3@gmail.com>
2023-08-13 22:58:28 -0500

    walker script

--- test_algs/baselines_run.sh
+++ test_algs/baselines_run.sh
@@ -19,16 +19,6 @@
 #         --init_temp 1e-2 \
 #         --hidden_policy "(64, 64)" --hidden_critic "(64, 64)"
 
-
-
-# option-gail
-# python3 test_algs/run_algs.py --alg_name ogail \
-#         --env_name MultiGoals2D_2-v0 --n_traj 300 --device "cpu" \
-#         --tag ogail_64_3e-5_value --seed 0 --dim_c 2 \
-#         --data_path "experts/MultiGoals2D_2-v0_500.pkl" --max_explore_step 3e6 \
-#         --use_option True \
-#         --mini_batch_size 500 --n_sample 5000 
-
 # # LunarLander-v2
 # python3 test_algs/run_algs.py --alg_name iql --env_type mujoco \
 #         --env_name LunarLander-v2 --n_traj 10 --device "cuda:0" \
@@ -42,14 +32,47 @@
 #         --hidden_policy "(64, 64)" --hidden_critic "(64, 64)"
 
 
-# CleanupSingle-v0
+# # CleanupSingle-v0
+# python3 test_algs/run_algs.py --alg_name iql --env_type mujoco \
+#         --env_name CleanupSingle-v0 --n_traj 100 --device "cpu" \
+#         --bounded_actor True --tag iql_256_3e-5 --n_sample 5000 \
+#         --iql_agent_name "softq" --method_regularize True --method_loss "value" \
+#         --data_path "experts/CleanupSingle-v0_100.pkl" \
+#         --max_explore_step 1e6 --mini_batch_size 256 --clip_grad_val 0 \
+#         --optimizer_lr_policy 1.e-4 --optimizer_lr_critic 1.e-4 --seed 0 \
+#         --learn_temp True --optimizer_lr_alpha 1.e-4 \
+#         --init_temp 1e-2 \
+#         --hidden_policy "(64, 64)" --hidden_critic "(64, 64)"
+
+# Hopper-v2
+# python3 test_algs/run_algs.py --alg_name iql --env_type mujoco \
+#         --env_name Hopper-v2 --n_traj 1 --device "cuda:0" \
+#         --bounded_actor True --tag iql_256_3e-5 --n_sample 5000 \
+#         --iql_agent_name "sac" --method_regularize True --method_loss "v0" \
+#         --data_path "experts/Hopper-v2_25.pkl" \
+#         --max_explore_step 1e6 --mini_batch_size 256 --clip_grad_val 0 \
+#         --optimizer_lr_policy 3.e-5 --optimizer_lr_critic 3.e-5 --seed 0 \
+#         --learn_temp True --optimizer_lr_alpha 3.e-5 \
+#         --init_temp 1e-2 \
+#         --hidden_policy "(64, 64)" --hidden_critic "(64, 64)"
+
+# Walker2d-v2
 python3 test_algs/run_algs.py --alg_name iql --env_type mujoco \
-        --env_name CleanupSingle-v0 --n_traj 100 --device "cpu" \
+        --env_name Walker2d-v2 --n_traj 5 --device "cpu" \
         --bounded_actor True --tag iql_256_3e-5 --n_sample 5000 \
-        --iql_agent_name "softq" --method_regularize True --method_loss "value" \
-        --data_path "experts/CleanupSingle-v0_100.pkl" \
+        --iql_agent_name "sac" --method_regularize True --method_loss "v0" \
+        --data_path "experts/Walker2d-v2_25.pkl" \
         --max_explore_step 1e6 --mini_batch_size 256 --clip_grad_val 0 \
-        --optimizer_lr_policy 1.e-4 --optimizer_lr_critic 1.e-4 --seed 0 \
-        --learn_temp True --optimizer_lr_alpha 1.e-4 \
+        --optimizer_lr_policy 3.e-5 --optimizer_lr_critic 3.e-4 --seed 0 \
+        --learn_temp True --optimizer_lr_alpha 3.e-4 \
         --init_temp 1e-2 \
-        --hidden_policy "(64, 64)" --hidden_critic "(64, 64)"
+        --hidden_policy "(256, 256)" --hidden_critic "(256, 256)"
+
+
+# option-gail
+# python3 test_algs/run_algs.py --alg_name ogail \
+#         --env_name MultiGoals2D_2-v0 --n_traj 300 --device "cpu" \
+#         --tag ogail_64_3e-5_value --seed 0 --dim_c 2 \
+#         --data_path "experts/MultiGoals2D_2-v0_500.pkl" --max_explore_step 3e6 \
+#         --use_option True \
+#         --mini_batch_size 500 --n_sample 5000 
--- test_algs/oiql_run.sh
+++ test_algs/oiql_run.sh
@@ -6,8 +6,7 @@
 #     tensorboard >= 2.2
 #     matplotlib >= 3.2
 
-###### for Hopper-v2 #####
-# Option-IQL:
+########################## Option-IQL:
 # python3 test_algs/run_algs.py --alg_name oiql --env_type mujoco \
 #         --env_name Hopper-v2 --n_traj 1 --device "cuda:0" \
 #         --bounded_actor True --tag oiqlstrm_64_3e-5_boundnnstd_extraD \
@@ -32,39 +31,40 @@
 #         --hidden_critic "(256, 256)" --demo_latent_infer_interval 5000 \
 #         --init_temp 0.2
 
-# Mental-IQL:
+########################### Mental-IQL:
+# Hopper-v2
+# python3 test_algs/run_algs.py --alg_name miql \
+#         --env_name Hopper-v2 --n_traj 1 --device "cpu" \
+#         --tag miql_256_3e-5 --seed 0 --dim_c 4 \
+#         --data_path "experts/Hopper-v2_25.pkl" --max_explore_step 1e6 \
+#         --mini_batch_size 256 --n_sample 5000  --stream_training True \
+#         --demo_latent_infer_interval 5000 --n_update_rounds 500 \
+#         --miql_update_strategy 1 --miql_tx_after_pi True \
+#         --miql_tx_method_loss "value" --miql_tx_method_regularize True \
+#         --miql_tx_optimizer_lr_critic 3.e-5 --miql_tx_init_temp 1e-2 \
+#         --miql_pi_optimizer_lr_critic 3.e-5 --miql_pi_optimizer_lr_alpha 3.e-5 \
+#         --miql_pi_optimizer_lr_policy 3.e-5 --miql_pi_init_temp 1e-2 \
+#         --miql_pi_learn_temp True --miql_pi_method_loss "v0" \
+#         --miql_pi_method_regularize True \
+#         --miql_tx_hidden_critic "(64, 64)" --miql_pi_hidden_critic "(64, 64)" \
+#         --miql_pi_hidden_policy "(64, 64)"
 
+# Walker2d-v2
 python3 test_algs/run_algs.py --alg_name miql \
-        --env_name Hopper-v2 --n_traj 1 --device "cpu" \
-        --tag miql_256_3e-5 --seed 0 --dim_c 4 \
-        --data_path "experts/Hopper-v2_25.pkl" --max_explore_step 1e6 \
-        --mini_batch_size 256 --n_sample 5000  --stream_training True \
-        --demo_latent_infer_interval 3000 --n_update_rounds 500 \
-        --miql_update_strategy 1 --miql_tx_after_pi True \
-        --miql_tx_method_loss "value" --miql_tx_method_regularize True \
-        --miql_tx_optimizer_lr_critic 3.e-5 --miql_tx_init_temp 1e-2 \
-        --miql_pi_optimizer_lr_critic 3.e-5 --miql_pi_optimizer_lr_alpha 3.e-5 \
-        --miql_pi_optimizer_lr_policy 3.e-5 --miql_pi_init_temp 1e-2 \
-        --miql_pi_learn_temp True --miql_pi_method_loss "value" \
-        --miql_pi_method_regularize True \
-        --miql_tx_hidden_critic "(64, 64)" --miql_pi_hidden_critic "(64, 64)" \
-        --miql_pi_hidden_policy "(64, 64)"
-
-python3 test_algs/run_algs.py --alg_name miql \
-        --env_name Walker2d-v2 --n_traj 5 --device "cpu" \
+        --env_name Walker2d-v2 --n_traj 5 --device "cuda:0" \
         --tag miql_256_3e-5 --seed 0 --dim_c 4 \
         --data_path "experts/Walker2d-v2_25.pkl" --max_explore_step 1e6 \
-        --mini_batch_size 256 --n_sample 5000  --stream_training True \
-        --demo_latent_infer_interval 3000 --n_update_rounds 500 \
+        --mini_batch_size 256 --n_sample 10000  --stream_training True \
+        --demo_latent_infer_interval 5000 --n_update_rounds 500 \
         --miql_update_strategy 1 --miql_tx_after_pi True \
         --miql_tx_method_loss "value" --miql_tx_method_regularize True \
-        --miql_tx_optimizer_lr_critic 3.e-5 --miql_tx_init_temp 1e-2 \
-        --miql_pi_optimizer_lr_critic 3.e-5 --miql_pi_optimizer_lr_alpha 3.e-5 \
+        --miql_tx_optimizer_lr_critic 1.e-4 --miql_tx_init_temp 1e-2 \
+        --miql_pi_optimizer_lr_critic 3.e-4 --miql_pi_optimizer_lr_alpha 3.e-4 \
         --miql_pi_optimizer_lr_policy 3.e-5 --miql_pi_init_temp 1e-2 \
-        --miql_pi_learn_temp True --miql_pi_method_loss "value" \
+        --miql_pi_learn_temp True --miql_pi_method_loss "v0" \
         --miql_pi_method_regularize True \
-        --miql_tx_hidden_critic "(64, 64)" --miql_pi_hidden_critic "(64, 64)" \
-        --miql_pi_hidden_policy "(64, 64)"
+        --miql_tx_hidden_critic "(256, 256)" --miql_pi_hidden_critic "(256, 256)" \
+        --miql_pi_hidden_policy "(256, 256)"
 
 # python3 test_algs/run_algs.py --alg_name miql \
 #         --env_name MultiGoals2D_2-v0 --n_traj 300 --device "cpu" \
