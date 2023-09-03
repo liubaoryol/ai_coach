ab2c22f360d4193ca7cabc09745e630ae551b046
Sangwon Seo <prizeworthy3@gmail.com>
2023-09-03 17:33:50 -0500

    update miql script

--- train_dnn/scripts/mujoco_miql_run.sh
+++ train_dnn/scripts/mujoco_miql_run.sh
@@ -2,7 +2,17 @@
 ########################### Mental-IQL:
 # Hopper-v2
 python3 train_dnn/run_algs.py alg=miql env=Hopper-v2 base_setting=mujoco_base \
-        tag=shortrun miql_pi_method_loss=v0 miql_pi_init_temp=1e-2
+        tag=shortrun_learntemp miql_pi_method_loss=v0 miql_pi_init_temp=0.01 \
+        miql_pi_learn_temp=True
+
+python3 train_dnn/run_algs.py alg=miql env=Hopper-v2 base_setting=mujoco_base \
+        tag=shortrun_temp0.01 miql_pi_method_loss=v0 miql_pi_init_temp=0.01
+
+python3 train_dnn/run_algs.py alg=miql env=Hopper-v2 base_setting=mujoco_base \
+        tag=shortrun_temp0.001 miql_pi_method_loss=v0 miql_pi_init_temp=0.001
+
+python3 train_dnn/run_algs.py alg=miql env=Hopper-v2 base_setting=mujoco_base \
+        tag=shortrun_temp0.1 miql_pi_method_loss=v0 miql_pi_init_temp=0.1
 
 # # Walker2d-v2
 # python3 train_dnn/run_algs.py alg=miql env=Walker2d-v2 base_setting=mujoco_base \
