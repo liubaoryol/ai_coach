9f5f78f88993082b9d2cfbd98624248177be7667
Sangwon Seo <prizeworthy3@gmail.com>
2023-09-05 19:49:00 -0500

    hopper script

--- train_dnn/scripts/mujoco_miql_run.sh
+++ train_dnn/scripts/mujoco_miql_run.sh
@@ -2,17 +2,17 @@
 ########################### Mental-IQL:
 # Hopper-v2
 python3 train_dnn/run_algs.py alg=miql env=Hopper-v2 base_setting=mujoco_base \
-        tag=shortrun_learntemp miql_pi_method_loss=v0 miql_pi_init_temp=0.01 \
-        miql_pi_learn_temp=True
+        tag=txtemp0.01 miql_pi_method_loss=v0 miql_pi_init_temp=0.02 miql_tx_init_temp=0.01
 
 python3 train_dnn/run_algs.py alg=miql env=Hopper-v2 base_setting=mujoco_base \
-        tag=shortrun_temp0.01 miql_pi_method_loss=v0 miql_pi_init_temp=0.01
+        tag=txtemp0.005 miql_pi_method_loss=v0 miql_pi_init_temp=0.02 miql_tx_init_temp=0.005
 
 python3 train_dnn/run_algs.py alg=miql env=Hopper-v2 base_setting=mujoco_base \
-        tag=shortrun_temp0.001 miql_pi_method_loss=v0 miql_pi_init_temp=0.001
+        tag=txtemp0.1 miql_pi_method_loss=v0 miql_pi_init_temp=0.02 miql_tx_init_temp=0.1
 
 python3 train_dnn/run_algs.py alg=miql env=Hopper-v2 base_setting=mujoco_base \
-        tag=shortrun_temp0.1 miql_pi_method_loss=v0 miql_pi_init_temp=0.1
+        tag=txtemp0.02 miql_pi_method_loss=v0 miql_pi_init_temp=0.02 miql_tx_init_temp=0.02
+
 
 # # Walker2d-v2
 # python3 train_dnn/run_algs.py alg=miql env=Walker2d-v2 base_setting=mujoco_base \
