7defe78ea246024593f40842dcb64d394c0f1f62
Sangwon Seo <prizeworthy3@gmail.com>
2023-09-10 14:50:44 -0500

    fix script

--- train_dnn/scripts/mujoco_iql_run.sh
+++ train_dnn/scripts/mujoco_iql_run.sh
@@ -1,15 +1,15 @@
 
 ########################### Mental-IQL:
 # Ant-v2
-python3 train_dnn/run_algs.py alg=iql env=Ant-v2 base_setting=mujoco_base \
+python3 train_dnn/run_algs.py alg=iql env=Ant-v2 base=mujoco_base \
         tag=rem50kskip5val method_loss=value init_temp=1e-3
 
-python3 train_dnn/run_algs.py alg=iql env=Ant-v2 base_setting=mujoco_base \
+python3 train_dnn/run_algs.py alg=iql env=Ant-v2 base=mujoco_base \
         tag=rem50kskip5v0 method_loss=v0 init_temp=1e-3
 
 # Humanoid-v2
-python3 train_dnn/run_algs.py alg=iql env=Humanoid-v2 base_setting=mujoco_base \
+python3 train_dnn/run_algs.py alg=iql env=Humanoid-v2 base=mujoco_base \
         tag=rem50kskip5v0 method_loss=v0 init_temp=1
 
-python3 train_dnn/run_algs.py alg=iql env=Humanoid-v2 base_setting=mujoco_base \
+python3 train_dnn/run_algs.py alg=iql env=Humanoid-v2 base=mujoco_base \
         tag=rem50kskip5val method_loss=value init_temp=1
