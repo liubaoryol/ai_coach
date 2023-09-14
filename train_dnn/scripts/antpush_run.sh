e3ebba1d6ae07469454e2c8681ce8dc8bb2b9625
Sangwon Seo <prizeworthy3@gmail.com>
2023-09-14 10:40:44 -0500

    antpush config

--- train_dnn/scripts/antpush_run.sh
+++ train_dnn/scripts/antpush_run.sh
@@ -10,11 +10,10 @@
 #         base=antpush_base tag=clip
 
 # intent-iql
-python3 train_dnn/run_algs.py env=AntPush-v0-clipped alg=miql \
-        base=antpush_base tag=infer5k_pival demo_latent_infer_interval=5e3
-
 # python3 train_dnn/run_algs.py env=AntPush-v0-clipped alg=miql \
 #         base=antpush_base tag=infer1k_pival demo_latent_infer_interval=1e3
 
-# python3 train_dnn/run_algs.py env=AntPush-v0-clipped alg=miql \
-#         base=antpush_base tag=infer5k_piv0 miql_pi_method_loss=v0
+# oiql
+python train_dnn/run_algs.py env=AntPush-v0-clipped alg=oiql \
+       base=antpush_base tag=val demo_latent_infer_interval=1e3 method_loss=v0 \
+       init_temp=1e-3
