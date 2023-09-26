864ca9b75cafacad8bda2a8bf7e98ab9ac2ce12c
Sangwon Seo <prizeworthy3@gmail.com>
2023-09-26 08:06:06 -0500

    update script

--- train_dnn/scripts/can_run_iql.sh
+++ train_dnn/scripts/can_run_iql.sh
@@ -1,17 +1,21 @@
 # IQL
  
 python train_dnn/run_algs.py alg=iql base=can_base \
-       env=RMPickPlaceCan-v0 tag=T001v0 \
+       env=RMPickPlaceCan-v0 tag=n2xT0010 \
        method_loss=v0 init_temp=0.01
 
 python train_dnn/run_algs.py alg=iql base=can_base \
-       env=RMPickPlaceCan-v0 tag=T01v0 \
-       method_loss=v0 init_temp=0.1
+       env=RMPickPlaceCan-v0 tag=n2xT003 \
+       method_loss=v0 init_temp=0.03
 
 python train_dnn/run_algs.py alg=iql base=can_base \
-       env=RMPickPlaceCan-v0 tag=T0005v0 \
-       method_loss=v0 init_temp=0.005
-
-python train_dnn/run_algs.py alg=iql base=can_base \
-       env=RMPickPlaceCan-v0 tag=T005v0 \
+       env=RMPickPlaceCan-v0 tag=n2xT005 \
        method_loss=v0 init_temp=0.05
+
+python train_dnn/run_algs.py alg=iql base=can_base \
+       env=RMPickPlaceCan-v0 tag=n2xT03 \
+       method_loss=v0 init_temp=0.3
+
+python train_dnn/run_algs.py alg=iql base=can_base \
+       env=RMPickPlaceCan-v0 tag=n2xT03 \
+       method_loss=v0 init_temp=0.1
--- train_dnn/scripts/can_run_miql.sh
+++ train_dnn/scripts/can_run_miql.sh
@@ -1,21 +1,17 @@
 # IQL
  
 python train_dnn/run_algs.py alg=miql base=can_base \
-       env=RMPickPlaceCan-v0 tag=Sv2Ttx001Tpi001 \
-       miql_tx_init_temp=0.01 miql_pi_init_temp=0.01
+       env=RMPickPlaceCan-v0 tag=n2xSv2Ttx001Tpi03 \
+       miql_tx_init_temp=0.01 miql_pi_init_temp=0.3
 
 python train_dnn/run_algs.py alg=miql base=can_base \
-       env=RMPickPlaceCan-v0 tag=Sv2Ttx001Tpi01 \
+       env=RMPickPlaceCan-v0 tag=n2xSv2Ttx001Tpi01 \
        miql_tx_init_temp=0.01 miql_pi_init_temp=0.1
 
 python train_dnn/run_algs.py alg=miql base=can_base \
-       env=RMPickPlaceCan-v0 tag=Sv2Ttx001Tpi003 \
+       env=RMPickPlaceCan-v0 tag=n2xSv2Ttx001Tpi003 \
        miql_tx_init_temp=0.01 miql_pi_init_temp=0.03
 
 python train_dnn/run_algs.py alg=miql base=can_base \
-       env=RMPickPlaceCan-v0 tag=Sv2Ttx001Tpi005 \
+       env=RMPickPlaceCan-v0 tag=n2xSv2Ttx001Tpi005 \
        miql_tx_init_temp=0.01 miql_pi_init_temp=0.05
-
-python train_dnn/run_algs.py alg=miql base=can_base \
-       env=RMPickPlaceCan-v0 tag=Sv2Ttx001Tpi0005 \
-       miql_tx_init_temp=0.01 miql_pi_init_temp=0.005
