0762c27b9e4e4d5ab5cd5def4168d34ce93d1fb9
Sangwon Seo <prizeworthy3@gmail.com>
2023-09-25 21:25:00 -0500

    update pickplace script

---
+++ train_dnn/scripts/can_run.sh
@@ -0,0 +1,32 @@
+# IQL
+python train_dnn/run_algs.py alg=iql base=can_base \
+       env=RMPickPlaceCan-v0 tag=T001val \
+       method_loss=value init_temp=0.01
+
+# python train_dnn/run_algs.py alg=iql base=can_base \
+#        env=RMPickPlaceCan-v0 tag=T01val \
+#        method_loss=value init_temp=0.1
+
+# python train_dnn/run_algs.py alg=iql base=can_base \
+#        env=RMPickPlaceCan-v0 tag=T1val \
+#        method_loss=value init_temp=1
+
+# python train_dnn/run_algs.py alg=iql base=can_base \
+#        env=RMPickPlaceCan-v0 tag=T0001val \
+#        method_loss=value init_temp=0.001
+ 
+# python train_dnn/run_algs.py alg=iql base=can_base \
+#        env=RMPickPlaceCan-v0 tag=T001v0 \
+#        method_loss=v0 init_temp=0.01
+
+# python train_dnn/run_algs.py alg=iql base=can_base \
+#        env=RMPickPlaceCan-v0 tag=T01v0 \
+#        method_loss=v0 init_temp=0.1
+
+# python train_dnn/run_algs.py alg=iql base=can_base \
+#        env=RMPickPlaceCan-v0 tag=T1v0 \
+#        method_loss=v0 init_temp=1
+
+# python train_dnn/run_algs.py alg=iql base=can_base \
+#        env=RMPickPlaceCan-v0 tag=T0001v0 \
+#        method_loss=v0 init_temp=0.001
