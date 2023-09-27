f987e76010751a100bcf4d91d80a0cff7c9984fc
Sangwon Seo <prizeworthy3@gmail.com>
2023-09-27 10:43:20 -0500

    update config

--- train_dnn/scripts/can_run_iql.sh
+++ train_dnn/scripts/can_run_iql.sh
@@ -1,7 +1,7 @@
 # IQL
  
 python train_dnn/run_algs.py alg=iql base=can_base \
-       env=RMPickPlaceCan-v0 tag=N512QdT01 init_temp=0.1
+       env=RMPickPlaceCan-v0 tag=N512x3LRhi init_temp=0.1 \
+       hidden_critic=[512,512,512] hidden_policy=[512,512,512] \
+       optimizer_lr_critic=1.e-3 optimizer_lr_policy=1e-4
 
-python train_dnn/run_algs.py alg=iql base=can_base \
-       env=RMPickPlaceCan-v0 tag=N512QdT008 init_temp=0.08
--- train_dnn/scripts/can_run_miql.sh
+++ train_dnn/scripts/can_run_miql.sh
@@ -1,17 +1,12 @@
-# IQL
+# MIQL
  
-python train_dnn/run_algs.py alg=miql base=can_base \
-       env=RMPickPlaceCan-v0 tag=Sv2N256_V0_QdT01 miql_pi_init_temp=0.1 \
-       miql_tx_method_loss=v0
+# python train_dnn/run_algs.py alg=miql base=can_base \
+#        env=RMPickPlaceCan-v0 tag=Sv2N256_Val_QdT01 miql_pi_init_temp=0.1 \
+#        miql_tx_method_loss=value
 
 python train_dnn/run_algs.py alg=miql base=can_base \
-       env=RMPickPlaceCan-v0 tag=Sv2N256_V0_QdT008 miql_pi_init_temp=0.08 \
-       miql_tx_method_loss=v0
-
-python train_dnn/run_algs.py alg=miql base=can_base \
-       env=RMPickPlaceCan-v0 tag=Sv2N256_Val_QdT008 miql_pi_init_temp=0.08 \
-       miql_tx_method_loss=value
-
-python train_dnn/run_algs.py alg=miql base=can_base \
-       env=RMPickPlaceCan-v0 tag=Sv2N256_Val_QdT01 miql_pi_init_temp=0.1 \
-       miql_tx_method_loss=value
+       env=RMPickPlaceCan-v0 tag=Sv2_N512x3LRhi miql_pi_init_temp=0.1 \
+       miql_tx_method_loss=value supervision=0.2 miql_pi_single_critic=False \
+       miql_pi_hidden_critic=[256,256,256] miql_pi_hidden_policy=[256,256,256] \
+       miql_tx_optimizer_lr_critic=1e-3 miql_pi_optimizer_lr_critic=1e-3 \
+       miql_pi_optimizer_lr_policy=1e-4
