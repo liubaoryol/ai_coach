7f1d15fa2b50ce427ab68087e0e9db25f6e0be68
Sangwon Seo <prizeworthy3@gmail.com>
2023-10-04 15:10:29 -0500

    update script

--- train_dnn/scripts/boxpush_run_2.sh
+++ train_dnn/scripts/boxpush_run_2.sh
@@ -1,11 +1,11 @@
 # MIQL
-python train_dnn/run_algs.py alg=miql base=MultiGoals2D_base \
+python train_dnn/run_algs.py alg=miql base=boxpush_base \
        env=EnvCleanup-v0 tag=sv2 supervision=0.2
 
-python train_dnn/run_algs.py alg=miql base=MultiGoals2D_base \
+python train_dnn/run_algs.py alg=miql base=boxpush_base \
        env=EnvMovers-v0 tag=sv2 supervision=0.2
 
-python train_dnn/run_algs.py alg=miql base=MultiGoals2D_base \
+python train_dnn/run_algs.py alg=miql base=boxpush_base \
        env=CleanupSingle-v0 tag=sv2 supervision=0.2
   
 # OGAIL
