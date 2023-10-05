9adc1037e9f64d9cc3627306c069ac8ae8c92ca8
Sangwon Seo <prizeworthy3@gmail.com>
2023-10-05 03:42:17 -0500

    boxpush scripts

--- train_dnn/scripts/boxpush_run_1.sh
+++ train_dnn/scripts/boxpush_run_1.sh
@@ -1,20 +1,31 @@
-# # IQL
-# python train_dnn/run_algs.py alg=iql base=boxpush_base \
-#        env=CleanupSingle-v0 tag=sv0 supervision=0.0 
+# IQL
+python train_dnn/run_algs.py alg=iql base=boxpush_base \
+       env=CleanupSingle-v0 tag=sv0seed1 seed=1 supervision=0.0 
 
-# python train_dnn/run_algs.py alg=iql base=boxpush_base \
-#        env=EnvMovers-v0 tag=sv0 supervision=0.0 
+python train_dnn/run_algs.py alg=iql base=boxpush_base \
+       env=EnvMovers-v0 tag=sv0seed1 seed=1 supervision=0.0 
 
-# python train_dnn/run_algs.py alg=iql base=boxpush_base \
-#        env=EnvCleanup-v0 tag=sv0 supervision=0.0 
+python train_dnn/run_algs.py alg=iql base=boxpush_base \
+       env=EnvCleanup-v0 tag=sv0seed1 seed=1 supervision=0.0 
+  
+# MIQL
+python train_dnn/run_algs.py alg=miql base=boxpush_base \
+       env=EnvCleanup-v0 tag=sv0seed1 seed=1 supervision=0.0
+
+python train_dnn/run_algs.py alg=miql base=boxpush_base \
+       env=EnvMovers-v0 tag=sv0seed1 seed=1 supervision=0.0
+
+python train_dnn/run_algs.py alg=miql base=boxpush_base \
+       env=CleanupSingle-v0 tag=sv0seed1 seed=1 supervision=0.0
+  
  
 # OIQL
 python train_dnn/run_algs.py alg=oiql base=MultiGoals2D_base \
-       env=CleanupSingle-v0 tag=sv2 supervision=0.2
+       env=CleanupSingle-v0 tag=sv0seed1 seed=1 supervision=0.0
 
 python train_dnn/run_algs.py alg=oiql base=MultiGoals2D_base \
-       env=EnvMovers-v0 tag=sv2 supervision=0.2
+       env=EnvMovers-v0 tag=sv0seed1 seed=1 supervision=0.0
 
 python train_dnn/run_algs.py alg=oiql base=MultiGoals2D_base \
-       env=EnvCleanup-v0 tag=sv2 supervision=0.2 
-  
+       env=EnvCleanup-v0 tag=sv0seed1 seed=1 supervision=0.0 
+
---
+++ train_dnn/scripts/boxpush_run_sv2.sh
@@ -0,0 +1,22 @@
+
+# MIQL
+python train_dnn/run_algs.py alg=miql base=boxpush_base \
+       env=EnvCleanup-v0 tag=sv2seed2 seed=2 supervision=0.2
+
+python train_dnn/run_algs.py alg=miql base=boxpush_base \
+       env=EnvMovers-v0 tag=sv2seed2 seed=2 supervision=0.2
+
+python train_dnn/run_algs.py alg=miql base=boxpush_base \
+       env=CleanupSingle-v0 tag=sv2seed2 seed=2 supervision=0.2
+  
+ 
+# OIQL
+python train_dnn/run_algs.py alg=oiql base=MultiGoals2D_base \
+       env=CleanupSingle-v0 tag=sv2seed2 seed=2 supervision=0.2
+
+python train_dnn/run_algs.py alg=oiql base=MultiGoals2D_base \
+       env=EnvMovers-v0 tag=sv2seed2 seed=2 supervision=0.2
+
+python train_dnn/run_algs.py alg=oiql base=MultiGoals2D_base \
+       env=EnvCleanup-v0 tag=sv2seed2 seed=2 supervision=0.2 
+
