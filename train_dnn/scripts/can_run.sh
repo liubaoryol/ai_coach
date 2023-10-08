4a1cf5589d0728dfc5f63f36b35495b970a6d870
Sangwon Seo <prizeworthy3@gmail.com>
2023-10-08 01:16:43 -0500

    update config

--- train_dnn/scripts/can_run.sh
+++ train_dnn/scripts/can_run.sh
@@ -1,19 +1,18 @@
-# IQL
- 
-python train_dnn/run_algs.py alg=iql base=can_base \
-       env=RMPickPlaceCan-v0 tag=N512x3LRhi
-
 # MIQL
 
 python train_dnn/run_algs.py alg=miql base=can_base \
-       env=RMPickPlaceCan-v0 tag=Sv2_N256x3LRhiClip
+       env=RMPickPlaceCan-v0 tag=Sv2_256x2T001 supervision=0.2 \
+       miql_tx_optimizer_lr_critic=3.e-4 miql_pi_optimizer_lr_critic=3.e-4 \
+       miql_pi_optimizer_lr_policy=3.e-5 miql_pi_init_temp=0.01
 
-# OIQL
 
-python train_dnn/run_algs.py alg=oiql base=can_base \
-       env=RMPickPlaceCan-v0 tag=Sv2_N512x3
 
-# OGAIL
+# # OIQL
 
-python train_dnn/run_algs.py alg=ogail base=can_base \
-       env=RMPickPlaceCan-v0 tag=Sv2_N512x2_N256x2
+# python train_dnn/run_algs.py alg=oiql base=can_base \
+#        env=RMPickPlaceCan-v0 tag=Sv2_N512x3
+
+# # OGAIL
+
+# python train_dnn/run_algs.py alg=ogail base=can_base \
+#        env=RMPickPlaceCan-v0 tag=Sv2_N512x2_N256x2
