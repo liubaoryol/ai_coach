5a7480c06f2a68f487493cfb715b8a67149bbe5e
Sangwon Seo <prizeworthy3@gmail.com>
2023-12-08 15:19:32 -0600

    fix config

--- web_app_v2/config.py
+++ web_app_v2/config.py
@@ -3,11 +3,12 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 # copy config.py to the instance directory and redefine below variables
 SECRET_KEY = 'sseo_data_col_v2'
 
-DATABASE = "./data/tw2020_db/tw2020.sqlite"
-SURVEY_PATH = "./data/tw2020_survey"
-TRAJECTORY_PATH = "./data/tw2020_trajectory"
-LATENT_PATH = "./data/tw2020_latent"
-USER_LABEL_PATH = "./data/tw2020_user_label"
+DATA_DIR = "./data"
+DATABASE = DATA_DIR + "/tw2020_db/tw2020.sqlite"
+SURVEY_PATH = DATA_DIR + "/tw2020_survey"
+TRAJECTORY_PATH = DATA_DIR + "/tw2020_trajectory"
+LATENT_PATH = DATA_DIR + "/tw2020_latent"
+USER_LABEL_PATH = DATA_DIR + "/tw2020_user_label"
 EXP_TYPE = "data_collection"  # data_collection | intervention
 
 DEBUG = False
